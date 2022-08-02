ffd1282ea56f5b90650528479f6d3444964ff0ff
ShivamY125 <89126908+ShivamY125@users.noreply.github.com>
2022-08-02 23:39:05 +0530

    Add files via upload

---
+++ longest_consecutive_sequence.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.*;
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        HashSet<Integer> h = new HashSet<>();
+        int res = 0;
+        int cur = 0;
+        for(int i=0;i<nums.length;i++){
+            h.add(nums[i]);
+        }
+        for(int i=0;i<nums.length;i++){
+        if(!h.contains(nums[i]-1)){
+            cur =1;
+        
+            while(h.contains(nums[i]+cur)){
+                cur++;
+            }
+           res = Math.max(res,cur); 
+        }
+        }
+        return res;
+    }
+}
